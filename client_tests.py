14560e86fc0cefe11320a420b90b09e9e08e7581
Jeff Espenschied <jeffesp@nexosis.com>
2017-07-10 10:44:01 -0400

    Fix tests and do some renaming.

--- client_tests.py
+++ client_tests.py
@@ -2,9 +2,11 @@ import unittest
 import nexosisclient
 import os
 
-class MyTestCase(unittest.TestCase):
+
+class BasicTests(unittest.TestCase):
     def setUp(self):
-        self.test_client = nexosisclient.NexosisClient(key=os.environ["NEXOSIS_API_TESTKEY"],uri=os.environ["NEXOSIS_API_TESTURI"])
+        self.test_client = nexosisclient.NexosisClient(key=os.environ["NEXOSIS_API_TESTKEY"],
+                                                       uri=os.environ["NEXOSIS_API_TESTURI"])
 
     def test_can_create_client_with_env_key(self):
         target = nexosisclient.NexosisClient()
@@ -12,12 +14,23 @@ class MyTestCase(unittest.TestCase):
 
     def test_can_override_client_base_uri(self):
         target = nexosisclient.NexosisClient(uri='https://api.uat.nexosisdev.com/v1')
-        self.assertEqual(target.uri,"https://api.uat.nexosisdev.com/v1")
+        self.assertEqual(target.uri, "https://api.uat.nexosisdev.com/v1")
 
     def test_api_returns_current_balance(self):
         actual = self.test_client.get_account_balance()
         self.assertIsNotNone(actual)
         self.assertTrue(actual > 0)
+
+
+class SessionIntegrationTests(unittest.TestCase):
+    def setUp(self):
+        pass
+
+
+class DataSetIntegrationTests(unittest.TestCase):
+    def setUp(self):
+        pass
+
 
 if __name__ == '__main__':
     unittest.main()
--- nexosisclient.py
+++ nexosisclient.py
@@ -1,29 +1,29 @@
-from Lib.test.test_importlib import import_
-
-import os
-import requests
 import json
+import os
 from datetime import datetime, date
+
+import requests
+
 
 class NexosisClient:
     NEXOSIS_API_KEY = 'NEXOSIS_API_KEY'
     SESSION_STATUS_HEADER = 'Nexosis-Session-Status'
 
-    def __init__(self, key = os.environ[NEXOSIS_API_KEY], uri='https://ml.nexosis.com/v1'):
+    def __init__(self, key=os.environ[NEXOSIS_API_KEY], uri='https://ml.nexosis.com/v1'):
         self.key = key
         if uri.endswith('/'):
             uri = uri[:-1]
         self.uri = uri
 
     @staticmethod
-    def __json_serial(obj):
+    def __json_serial__(obj):
         """JSON serializer for datetime since it is not serializable by default json code"""
         if isinstance(obj, (datetime, date)):
             serial = obj.isoformat()
             return serial
         raise TypeError("Type %s not serializable" % type(obj))
 
-    def __generate_headers(self):
+    def __generate_headers__(self):
         return {
             'api-key': self.key,
             'User-Agent': 'Nexosis-Python-API-Client/1.0',
@@ -31,8 +31,8 @@ class NexosisClient:
             'Accept': 'application/json'
         }
 
-    def __create_session(self, data, action_type, dataset_name, target_column, event_name,
-                         start_date, end_date, column_metadata = {}, callback_url = None):
+    def __create_session__(self, data, action_type, dataset_name, target_column, event_name,
+                           start_date, end_date, column_metadata={}, callback_url=None):
         resp = requests.post('%s/sessions/%s' % (self.uri, action_type),
                              params={
                                  'dataSetName': dataset_name,
@@ -41,38 +41,39 @@ class NexosisClient:
                                  'startDate': start_date,
                                  'endDate': end_date,
                                  'callbackUrl': callback_url},
-                             data=json.dumps(data, default = NexosisClient.__json_serial),
-                             headers=self.__generate_headers())
+                             data=json.dumps(data, default=NexosisClient.__json_serial__),
+                             headers=self.__generate_headers__())
 
         return resp.json(), resp.status_code, resp.headers
 
     def get_account_balance(self):
+        """get_account_balance"""
         balance_url = self.uri + "/data"
-        response = requests.get(balance_url, headers=self.__generate_headers())
+        response = requests.get(balance_url, headers=self.__generate_headers__())
         if response.status_code == 200:
-            return response.headers['nexosis-account-balance']
-        else:
-            response.raise_for_status()
+            header = response.headers['nexosis-account-balance']
+            if header is not None:
+                value = header.split(' ')[0]
+                return float(value)
 
-    def create_forecast(self,dataset_name,start_date,end_date,column_metadata):
-        return None
+        response.raise_for_status()
 
-    def create_forecast(self, data, start_date, end_date, callback_url=None):
+    def create_forecast(self, data, target_column, start_date, end_date, callback_url=None):
         """create_forecast"""
-        return self.__create_session(
+        return self.__create_session__(
             data, 'forecast', None, target_column, None, start_date, end_date, callback_url)
 
     def analyze_impact(self, data, target_column, event_name, start_date, end_date, callback_url=None):
         """analyze_impact"""
-        return self.__create_session(
+        return self.__create_session__(
             data, 'impact', None, target_column, event_name, start_date, end_date, callback_url)
 
     def get_status(self, session_id):
         """get_status"""
-        resp = requests.head('%s/sessions/%s' % (self.uri, session_id), headers=self.__generate_headers())
+        resp = requests.head('%s/sessions/%s' % (self.uri, session_id), headers=self.__generate_headers__())
         return None, resp.status_code, resp.headers
 
     def get_results(self, session_id):
         """get_results"""
-        resp = requests.get('%s/sessions/%s/results' % (self.uri, session_id), headers=self.__generate_headers())
+        resp = requests.get('%s/sessions/%s/results' % (self.uri, session_id), headers=self.__generate_headers__())
         return resp.json(), resp.status_code, resp.headers
