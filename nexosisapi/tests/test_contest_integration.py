6dcb2742726296dab93caa96a348bbfca6f8ad19
Jeff Gabriel <gabe19@yahoo.com>
2017-12-21 13:26:53 -0500

    removing debugging lines

--- nexosisapi/tests/test_contest_integration.py
+++ nexosisapi/tests/test_contest_integration.py
@@ -12,14 +12,8 @@ class ContestIntegrationTests(unittest.TestCase):
     def setUpClass(cls):
         cls.test_client = Client(key=os.environ['NEXOSIS_PAID_API_TESTKEY'],
                                  uri=os.environ['NEXOSIS_API_TESTURI'])
-        if not os.environ['NEXOSIS_PAID_API_TESTKEY']:
-            print('Paid API key was not found')
-        try:
-            current_sessions = cls.test_client.sessions.list('', page_size=100)
-            cls.completed_session = next((s for s in current_sessions if s.status == Status.completed), None)
-        except ClientError as err:
-            print(err.message, file=sys.stderr)
-            raise
+        current_sessions = cls.test_client.sessions.list('', page_size=100)
+        cls.completed_session = next((s for s in current_sessions if s.status == Status.completed), None)
 
     def test_can_get_contest(self):
         actual = self.test_client.sessions.get_contest(self.completed_session.session_id)
