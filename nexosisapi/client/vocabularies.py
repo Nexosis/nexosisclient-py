e9ecd7d5a86e6e33719bd85aa63a510d72b94402
Craig Vermeer <craig@vermeers.net>
2018-01-26 15:17:16 -0500

    Vocabulary tests

--- nexosisapi/client/vocabularies.py
+++ nexosisapi/client/vocabularies.py
@@ -1,6 +1,7 @@
 from nexosisapi.paged_list import PagedList
 from nexosisapi.vocabulary_summary import VocabularySummary
-
+from nexosisapi.vocabulary import Vocabulary
+from nexosisapi.word import Word
 
 
 class Vocabularies(object):
@@ -18,7 +19,7 @@ class Vocabularies(object):
         :param str created_from_session: optional filter to limit vocabularies to those built from a particular session
         :param int page_number: optional zero-based page number of results to retrieve
         :param int page_size: optional count of results to retrieve in each page (default 50, max 1000).
-        :return: a `list` of VocabularySummary objects representing the views stored
+        :return: a `list` of VocabularySummary objects representing the vocabularies built
         :rtype: list
         """
 
@@ -34,4 +35,28 @@ class Vocabularies(object):
 
         return PagedList.from_response(
             [VocabularySummary(item) for item in listing.get('items', [])],
+            listing)
+
+
+    def get(self, vocabulary_id, type=None, page_number=0, page_size=50):
+        """Get the list of vocabulary words in a vocabulary
+
+        :param str vocabulary_id: the vocabulary id whose words should be retrieved
+        :param str type: optional filter to limit words to only Words or StopWords
+        :param int page_number: optional zero-based page number of results to retrieve
+        :param int page_size: optional count of results to retrieve in each page (default 50, max 1000).
+        :return: a `list` of Word objects representing the words on the vocabulary
+        :rtype: list
+        """
+
+        params = {'page': page_number, 'pageSize': page_size}
+        if type:
+            params['type'] = type
+
+
+        listing = self._client.request('GET', '/vocabulary/%s' % vocabulary_id,
+                                       params=params)
+
+        return Vocabulary.from_response(
+            [Word(item) for item in listing.get('items', [])],
             listing)
--- nexosisapi/tests/test_vocabulary.py
+++ nexosisapi/tests/test_vocabulary.py
@@ -1,4 +1,5 @@
 from nexosisapi.vocabulary_summary import VocabularySummary
+from nexosisapi.vocabulary import Vocabulary
 from nexosisapi.data_source_type import DataSourceType
 from nexosisapi.word import Word, WordType
 import unittest
@@ -44,3 +45,24 @@ class VocabularyWordUnitTests(unittest.TestCase):
 
     def test_parses_rank(self):
         self.assertEqual(1, self.word.rank)
+
+class VocabularyUnitTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        message = {'id': '12345', 'items': [{'text': 'all work and no play make jack a dull boy', 'type': 0, 'rank': 1}, {'text': 'all work and no play make jack a dull boy', 'type': 0, 'rank': 1}], 'pageNumber': 1, 'totalPages': 2, 'pageSize': 3, 'totalCount': 10}
+        cls.vocab = Vocabulary.from_response([Word(w) for w in message.get('items')], message)
+
+    def test_parses_id(self):
+        self.assertEqual('12345', self.vocab.vocabulary_id)
+
+    def test_parses_items(self):
+        self.assertEqual(2, len(self.vocab))
+
+    def test_parses_paging_stuff(self):
+        self.assertEqual(1, self.vocab.page_number)
+        self.assertEqual(2, self.vocab.total_pages)
+        self.assertEqual(3, self.vocab.page_size)
+        self.assertEqual(10, self.vocab.item_total)
+
+
+
